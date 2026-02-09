750c7588579298258213db848b63ef4c1ef96bc6
Chí Hào <NguyenTrungChiHao8809@gmail.com>
2026-02-09 14:28:52 +0700

    hday

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -3,6 +3,7 @@ class ApiConstants {
   static const String mapboxToken = 'pk.eyJ1IjoidHJ1bmdjaGloYW8iLCJhIjoiY21rYzRid2RlMGF2YzNucG8yM3h4cTZzYyJ9.62ETdJoM5foxkcPAxFe7Wg';
   static const String mapboxBaseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places';
   static const String backendUrl = 'http://192.168.1.5:8080/api/locations';
+  // static const String backendUrl = 'http://10.0.2.2:8080/api/locations';
 
   // Base URL gốc của server
   static const String baseUrl = "http://192.168.1.250:8080/api";
